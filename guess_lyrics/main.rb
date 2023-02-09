e1a2c1f303d6220c9f912c64b2189f26eb0f6373
James <jamesrobertsongroves@gmail.com>
2023-02-09 17:00:58 +0000

    fixed styling

--- guess_lyrics/main.rb
+++ guess_lyrics/main.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'json'
 
 class GuessingGame
-  @@dificulties = ["easy", "medium", "hard"]
+  @@dificulties = %w[easy medium hard]
   @@games_ever_played = 0
 
   def initialize
@@ -12,24 +14,23 @@ class GuessingGame
     @dificulty = nil
 
     # pick a lyric from the JSON file
-    f = File.open("data.json", "r")
+    f = File.open('data.json', 'r')
     data = f.read
     j = JSON.parse(data)
-    @lyrics = j["data"].sample
-    puts @lyrics
+    @lyrics = j['data'].sample
   end
 
   def play
     pick_difficulty
     puts "\nYou have selected #{@difficulty} mode.\n"
-    self.send("#{@difficulty}_mode")
+    send("#{@difficulty}_mode")
   end
 
   def pick_difficulty
     until @@dificulties.include?(@difficulty)
       puts "Welcome to the Lyric Guessing Game! Choose difficulty:\n\n1. Easy (Choices are provided)\n2. Medium (Guess the word)\n3. Hard (Guess the line)\n4. Random\n\nWhat is your choice (1-4)?\n"
       @difficulty = gets.chomp
-      game_over if @difficulty == "exit"
+      game_over if @difficulty == 'exit'
 
       @difficulty = @difficulty.to_i
       if @difficulty == 4
@@ -37,7 +38,7 @@ class GuessingGame
       elsif @difficulty.between?(1, 3)
         @difficulty = @@dificulties[@difficulty - 1]
       else
-        puts "Invalid choice. Try again."
+        puts 'Invalid choice. Try again.'
         @difficulty = nil
       end
     end
@@ -46,9 +47,9 @@ class GuessingGame
   def easy_mode
     guess = 0
     until guess.between?(1, 3)
-      puts "#{@lyrics['prompt_start'] + @lyrics[@difficulty]}"
+      puts (@lyrics['prompt_start'] + @lyrics[@difficulty]).to_s
       guess = gets.chomp
-      if guess == "exit"
+      if guess == 'exit'
         game_over
       else
         guess = guess.to_i
@@ -68,28 +69,28 @@ class GuessingGame
 
   def get_guess
     guess = 0
-    puts "#{@lyrics['prompt_start'] + @lyrics[@difficulty]}"
+    puts (@lyrics['prompt_start'] + @lyrics[@difficulty]).to_s
     guess = gets.chomp
-    game_over if guess == "exit"
+    game_over if guess == 'exit'
 
     guess
   end
 
   def validate_guess(guess)
     if guess == @lyrics["#{@difficulty}_answer"]
-      puts "Correct!"
+      puts 'Correct!'
     else
       puts @lyrics['lose_text']
     end
   end
 
   def game_over
-    puts "Sorry to see you go! Have a good day!"
+    puts 'Sorry to see you go! Have a good day!'
     exit
   end
 end
 
 game = GuessingGame.new.play
 
-puts "Would you like to play again?"
-gets.chomp.downcase[0] == "y" ? game = GuessingGame.new.play : exit
+puts 'Would you like to play again?'
+gets.chomp.downcase[0] == 'y' ? game = GuessingGame.new.play : exit
